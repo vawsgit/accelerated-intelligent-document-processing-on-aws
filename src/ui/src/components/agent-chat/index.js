b3f895002a1de17e59b2ecf756847e6cb57faaca
Wes [C] Morishita <morishwe@amazon.com>
2025-11-21 18:19:24 +0000

    Improvements to Agent Chat Experience

--- src/ui/src/components/agent-chat/index.js
+++ src/ui/src/components/agent-chat/index.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import AgentChatLayout from './AgentChatLayout';
+import AgentToolComponent from './AgentToolComponent';
 
 export default AgentChatLayout;
-export { AgentChatLayout };
+export { AgentChatLayout, AgentToolComponent };
